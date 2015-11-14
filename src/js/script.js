f6c1b2c9c31bc3da7c95f877a5c12c4ba727c490
Loris Bettazza <loriteck@yahoo.it>
2015-11-14 01:24:20 +0100

    Queue changes, random placeholder from the JS randomator
    - Repeat button now in player instead of the queue (it will change to
    repeat loop or current song or nothing based on how many times it’s
    clicked)
    - Removed placeholders in the queue because JS is going to fill in some
    random contents
    - Extracted the queue to it’s own .sass file
    - Other minor stuff

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
 
     // Cache DOM
     var $burger = $('.burger'),
-        $sidebarCloseButton = $('.sidebar .close');
+        $sidebarCloseButton = $('.sidebar .close'),
+        $songInPlayer = $('.player > .song'),
+        $queueButton = $songInPlayer.find('.queue-button'),
+        $queueList = $songInPlayer.find('.queue-list');
 
     // Event binding
     $(window).on('resize', function() {
@@ -17,4 +20,55 @@ $(document).ready(function() {
     $sidebarCloseButton.on('click', function() {
         Sidebar.hide();
     });
+
+    // Init
+    if ($queueList.length > 0) {
+        Randomator.getData(function(data) {
+            var i,
+                itemNumber = Math.floor(Math.random() * 20) + 1,
+                songsString = '';
+
+                for (i=0; i<itemNumber; i++) {
+                    var song = Randomator.getSong(data);
+
+                    songsString += '' +
+                        '<li class="song' + (i === 0 ? ' active' : '') + '">' +
+                            '<a href="#">' +
+                                '<img class="thumbnail" src="img/covers/' + song.imageUrl + '">' +
+                            '</a>' +
+                            '<div class="details">' +
+                                '<a class="title truncate" href="#" title="' + song.name + '">' + song.name + '</a>' +
+                                '<a class="artist truncate" href="#" title="' + song.artist + '">' + song.artist + '</a>' +
+                            '</div>' +
+                            '<div class="icon-bars">' +
+                                '<div class="bar"></div>' +
+                                '<div class="bar"></div>' +
+                                '<div class="bar"></div>' +
+                            '</div>' +
+                            '<button class="player-button">' +
+                                '<i class="material-icons tiny">more_vert</i>' +
+                            '</button>' +
+                            '<button class="player-button">' +
+                                '<i class="material-icons tiny">clear</i>' +
+                            '</button>' +
+                        '</li>';
+
+                    if (i === 0) {
+                        var songInPlayer = '';
+                            songInPlayer += '' +
+                                '<a href="#">' +
+                                    '<img class="thumbnail" src="img/covers/' + song.imageUrl + '">' +
+                                '</a>' +
+                                '<div class="details">' +
+                                    '<a class="title truncate" href="#" title="' + song.name + '">' + song.name + '</a>' +
+                                    '<a class="artist truncate" href="#" title="' + song.artist + '">' + song.artist + '</a>' +
+                                '</div>';
+
+                        $queueButton.before(songInPlayer);
+                    }
+                }
+
+            $queueList.append(songsString);
+        });
+    }
 });
